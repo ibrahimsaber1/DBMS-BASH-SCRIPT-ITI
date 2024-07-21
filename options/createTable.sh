414860870f48d23f3f1aa4891f10b78e6e781728
Ibrahim Saber <ibrahimsaber622@gmail.com>
2024-07-21 14:27:46 +0300

    update createTable Function to make metadate file

--- options/createTable.sh
+++ options/createTable.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-create_table () {
+create_table() {
     echo -e "\nCreate Table\n"
     read -p "Please enter the name of the table: " table_name
+    
+    # Trim leading/trailing spaces
+    table_name=$(echo "$table_name" | xargs)
     
     # Check if the table name is empty
     if [ -z "$table_name" ]; then
         echo -e "\nPlease enter a correct name\n"
         create_table # call the function again
-    
+
     # Check if the table name contains spaces
     elif [[ "$table_name" = *[[:space:]]* ]]; then
         echo -e "\nTable name cannot contain spaces\n"
         create_table # call the function again
-    
+
     # Check if the table already exists
-    elif [ -f "$table_name" ]; then
+    elif [ -f "$table_name.table" ]; then
         echo -e "\nThis table name already exists\n"
         create_table # call the function again
-    
+
     # Check if the table name starts with an alphabetic character
     elif [[ "$table_name" == [a-zA-Z]* ]]; then
         echo -e "\nTable created successfully\n"
-        touch "$table_name"
-        
+        touch "$table_name.table"
+        touch "metaData_$table_name"
+
         read -p "Enter number of columns: " colnumber
         
-        # Write metadata to the table file
-        echo "general : Table Name : $table_name" > "$table_name"
-        echo "general : Number of Columns : $colnumber" >> "$table_name"
+        # Write metadata to the metadata file
+        echo "general : Table Name : $table_name" > "metaData_$table_name"
+        echo "general : Number of Columns : $colnumber" >> "metaData_$table_name"
         
         index=1
         while [ $index -le $colnumber ]; do
@@ -37,8 +41,8 @@ create_table () {
                 echo -e "The first column is the primary key"
                 echo -e "===================================="
                 read -p "Enter column $index name: " colname
-                echo "primary key : $index : $colname : int" >> "$table_name"
-                printf "$colname:" >> "$table_name"
+                echo "primary key : $index : $colname : int" >> "metaData_$table_name"
+                printf "$colname:" >> "$table_name.table"
             else
                 echo ""
                 read -p "Enter column $index name: " colname
@@ -47,17 +51,17 @@ create_table () {
                 select datatype in "int" "str"; do
                     case $datatype in
                         int ) colType="int"; break;;
-                        str ) colType="string"; break;;
+                        str ) colType="str"; break;;
                         * ) echo -e "\nWrong Choice!!!";;
                     esac
                 done
-                echo "Not primary key : $index : $colname : $datatype" >> "$table_name"
-                printf "$colname:" >> "$table_name"
+                echo "Not primary key : $index : $colname : $colType" >> "metaData_$table_name"
+                printf "$colname:" >> "$table_name.table"
             fi
             ((index = $index + 1))
         done
-        printf "\n" >> "$table_name"
-        printf "=================\n" >> "$table_name"
+        printf "\n" >> "$table_name.table"
+        printf "=================\n" >> "$table_name.table"
         echo -e "\nTable created successfully\n"
     else
         echo -e "\nPlease enter a correct name\n"
